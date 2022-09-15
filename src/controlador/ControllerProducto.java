eb0f2757d271344749541d3aa5182c8d07286344
panqsito <jhudyt.gaona.est@tecazuay.edu.ec>
2022-09-14 23:09:46 -0500

    Modelo y Controller Producto

--- src/controlador/ControllerProducto.java
+++ src/controlador/ControllerProducto.java
@@ -1,15 +1,180 @@
 package controlador;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.List;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+import javax.xml.ws.Holder;
+import modelo.ModeloProducto;
+import modelo.Producto;
+import vista.ViewAdministrador;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Laptop
  */
 public class ControllerProducto {
-    
+
+    private ModeloProducto model;
+    private ViewAdministrador vist;
+    int filas;
+
+    public ControllerProducto(ModeloProducto model, ViewAdministrador vist) {
+        this.model = model;
+        this.vist = vist;
+        vist.setVisible(true);
+        vist.setLocationRelativeTo(null);
+        cargaDatos();
+    }
+
+    public void iniciaControl() {
+        cargaDatos();
+        vist.getBtnbuscarProd().addActionListener(l -> cargaDatos());
+        //vist.getb.addActionListener(ll -> salir());
+        vist.getBtnCREARPROD().addActionListener(lc -> crearProducto());
+        vist.getBtnELIMINARPROD().addActionListener(le -> eliminarProducto());
+    }
+
+    private void crearProducto() {
+
+        int cantidad = (int) vist.getjSpinnerCantidad().getValue();
+        String tipoprod = vist.getTxtTipoProducto().getText();
+        int iddonaprod = vist.getCbxIdDonacion().getSelectedIndex();
+        int idproyprod = vist.getCbxProyecto().getSelectedIndex();
+
+        ModeloProducto producto = new ModeloProducto();
+        producto.setCantprod(cantidad);
+        producto.setTipoprod(tipoprod);
+        producto.setIddonaprod(iddonaprod);
+        producto.setIdproyprod(idproyprod);
+
+        if (producto.setProducto()) {
+            JOptionPane.showMessageDialog(vist, "PERSONA CREADA :)");
+            cargaDatos();
+        } else {
+            JOptionPane.showMessageDialog(vist, "NO SE PUDO CREAR LA PERSONA");
+        }
+    }
+
+    private void eliminarProducto() {
+        ModeloProducto producto = new ModeloProducto();
+        try {
+            int m = vist.getjTableDatosProductos().getSelectedRow();
+            int id = (int) vist.getjTableDatosProductos().getValueAt(m, 0);
+            if (vist.getjTableDatosProductos().getSelectedRow() >= 0) {
+                DefaultTableModel dtm = (DefaultTableModel) vist.getjTableDatosProductos().getModel();
+                int select = JOptionPane.showConfirmDialog(vist, "¿ESTÁS SEGUR@ DE ELIMINAR A ESA PERSONA?");
+                if (select == JOptionPane.YES_OPTION) {
+                    dtm.removeRow(vist.getjTableDatosProductos().getSelectedRow());
+                    producto.deletePersona(id);
+                }
+            } else {
+                JOptionPane.showMessageDialog(null, "Seleccione una producto en la tabla");
+            }
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            JOptionPane.showMessageDialog(vist, "ERROR, Selecciona una producto de la tabla");
+        }
+    }
+
+    public void cargarDatosModifProd() {
+        try {
+            int filaSelecionada = vist.getjTableDatosProductos().getSelectedRow();
+            System.out.println(vist.getjTableDatosProductos().getValueAt(filaSelecionada, 0).toString());
+            int ide = (int) vist.getjTableDatosProductos().getValueAt(filaSelecionada, 0);
+
+            List<Producto> lista = model.productoModif(ide);
+            for (int i = 0; i < lista.size(); i++) {
+
+                vist.getjSpinnerCantidad().setValue(lista.get(i).getCantprod());
+                vist.getTxtTipoProducto().setText(lista.get(i).getTipoprod());
+                vist.getCbxIdDonacion().setSelectedIndex(lista.get(i).getIddonaprod());
+                vist.getCbxProyecto().setSelectedIndex(lista.get(i).getIdproyprod());
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void tbl() {
+        try {
+            int edit = vist.getjTableDatosProductos().getSelectedRow();
+            vist.getjSpinnerCantidad().setValue(String.valueOf(vist.getjTableDatosProductos().getValueAt(edit, 1)));
+            vist.getTxtTipoProducto().setText((String) vist.getjTableDatosProductos().getValueAt(edit, 2));
+            vist.getCbxIdDonacion().setSelectedIndex((int) vist.getjTableDatosProductos().getValueAt(edit, 3));
+            vist.getCbxProyecto().setSelectedIndex((int) vist.getjTableDatosProductos().getValueAt(edit, 4));
+        } catch (Exception E) {
+            System.out.println(E);
+
+        }
+    }
+
+    private void cargaDatos() {
+
+        DefaultTableModel tabla;
+        tabla = (DefaultTableModel) vist.getjTableDatosProductos().getModel();
+        tabla.setNumRows(0);
+
+        List<Producto> listap = model.getProducto();
+        Holder<Integer> i = new Holder<>(0);
+
+        listap.stream().forEach(per -> {
+            tabla.addRow(new Object[5]);
+            vist.getjTableDatosProductos().setValueAt(per.getIdprod(), i.value, 0);
+            vist.getjTableDatosProductos().setValueAt(per.getCantprod(), i.value, 1);
+            vist.getjTableDatosProductos().setValueAt(per.getTipoprod(), i.value, 2);
+            vist.getjTableDatosProductos().setValueAt(per.getIddonaprod(), i.value, 3);
+            vist.getjTableDatosProductos().setValueAt(per.getIdproyprod(), i.value, 4);
+            i.value++;
+        });
+    }
+
+    private void limpiar() {
+        vist.getTxtTipoProducto().setText("");
+        vist.getCbxIdDonacion().setSelectedIndex(0);
+        vist.getCbxProyecto().setSelectedIndex(0);
+        vist.getjSpinnerCantidad().setValue(0);
+    }
+
+    KeyListener kyl = new KeyListener() {
+        @Override
+        public void keyTyped(KeyEvent e) {
+
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            if (e.getSource() == vist.getTxtBsqProductos()) {
+
+                String tipo = vist.getTxtBsqProductos().getText();
+
+                DefaultTableModel tabla;
+                tabla = (DefaultTableModel) vist.getjTableDatosProductos().getModel();
+                tabla.setNumRows(0);
+                List<Producto> listap = model.buscaProducto(tipo);
+
+                Holder<Integer> i = new Holder<>(0);
+                listap.stream().forEach(pe -> {
+                    tabla.addRow(new Object[5]);
+                    vist.getjTableDatosProductos().setValueAt(pe.getIdprod(), i.value, 0);
+                    vist.getjTableDatosProductos().setValueAt(pe.getCantprod(), i.value, 1);
+                    vist.getjTableDatosProductos().setValueAt(pe.getTipoprod(), i.value, 2);
+                    vist.getjTableDatosProductos().setValueAt(pe.getIddonaprod(), i.value, 3);
+                    vist.getjTableDatosProductos().setValueAt(pe.getIdproyprod(), i.value, 4);
+                    i.value++;
+                });
+            }
+
+        }
+    };
 }
--- src/modelo/ModeloProducto.java
+++ src/modelo/ModeloProducto.java
@@ -5,10 +5,116 @@
  */
 package modelo;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Laptop
  */
-public class ModeloProducto {
-    
+public class ModeloProducto extends Producto{
+
+    ModelPGConection mpgc = new ModelPGConection();
+
+    public ModeloProducto() {
+    }
+
+    public ModeloProducto(int idprod, int cantprod, String tipoprod, int iddonaprod, int idproyprod) {
+        super(idprod, cantprod, tipoprod, iddonaprod, idproyprod);
+    }
+
+    // manejar los datos de la conecxion de base dedatos 
+    public boolean setProducto() {
+        String sql = "Insert into producto (cantidadprod,tipoprod,iddonaprod,idproyprod)"
+                + "values ('" + getCantprod() + "'"
+                + ",'" + getTipoprod() + "'"
+                + ",'" + getIddonaprod() + "'"
+                + ",'" + getIdproyprod() + "')";
+        return mpgc.accion(sql);// se ejecuta el insert
+    }
+
+    public boolean updateProducto(int id) {
+        String sql = "update producto set cantidadprod='" + getCantprod()
+                + "',tipoprod='" + getTipoprod() + "',iddonaprod='" + getIddonaprod()
+                + "',idproyprod='" + getIdproyprod()
+                + "' where idprod='" + id + "'; ";
+        return mpgc.accion(sql);// se ejecuta el update
+    }
+
+    public boolean deletePersona(int id) {
+        String sql = "delete from producto where idprod='" + id + "'; ";
+        return mpgc.accion(sql);// se ejecuta el delete
+    }
+
+    public List<Producto> getProducto() {
+        List<Producto> listprod = new ArrayList<>();
+        String sql = "select * from producto";
+        ResultSet rs = mpgc.consulta(sql);
+        byte[] bytea;
+        try {
+            while (rs.next()) {
+                Producto p = new Producto();
+                p.setIdprod(rs.getInt("idprod"));
+                p.setCantprod(rs.getInt("cantidadprod"));
+                p.setTipoprod(rs.getString("tipoprod"));
+                p.setIddonaprod(rs.getInt("iddonaprod"));
+                p.setIdproyprod(rs.getInt("idproyprod"));
+                listprod.add(p);
+            }
+        } catch (SQLException e) {
+            Logger.getLogger(ModelPGConection.class.getName()).log(Level.SEVERE, null, e);
+        }
+
+        return listprod;
+    }
+
+    public List<Producto> buscaProducto(String texto) {
+        List<Producto> listproducto = new ArrayList<>();
+        String sql = "select * from productos where tipoprod LIKE '%" + texto + "%'";
+        System.out.println(sql);
+        ResultSet rs = mpgc.consulta(sql);
+        try {
+            while (rs.next()) {
+                Producto p = new Producto();
+                p.setIdprod(rs.getInt("idprod"));
+                p.setCantprod(rs.getInt("cantidadprod"));
+                p.setTipoprod(rs.getString("tipoprod"));
+                p.setIddonaprod(rs.getInt("iddonaprod"));
+                p.setIdproyprod(rs.getInt("idproyprod"));
+                listproducto.add(p);
+            }
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return listproducto;
+    }
+
+    public List<Producto> productoModif(int texto) {
+        List<Producto> listProducto = new ArrayList<>();
+        String sql = "Select * from producto where idprod = '" + texto + "'";
+        byte[] bytea;
+        System.out.println(sql);
+        ResultSet rs = mpgc.consulta(sql);
+        try {
+            while (rs.next()) {
+                Producto p = new Producto();
+                p.setIdprod(rs.getInt("idprod"));
+                p.setCantprod(rs.getInt("cantidadprod"));
+                p.setTipoprod(rs.getString("tipoprod"));
+                p.setIddonaprod(rs.getInt("iddonaprod"));
+                p.setIdproyprod(rs.getInt("idproyprod"));
+                listProducto.add(p);
+            }
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return listProducto;
+    }
+
 }
